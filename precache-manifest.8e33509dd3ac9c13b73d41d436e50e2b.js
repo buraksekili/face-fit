8dcec5baff68cb379fd4985e7d65116fbb40fed7
Burak Sekili <buraksekili@gmail.com>
2020-07-13 00:42:24 +0300

    Updates

---
+++ precache-manifest.8e33509dd3ac9c13b73d41d436e50e2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e4f9fd400514c5ee3db36c2b10838b1",
+    "url": "/crop-image/index.html"
+  },
+  {
+    "revision": "c9773eb23004ccb4c763",
+    "url": "/crop-image/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "c9773eb23004ccb4c763",
+    "url": "/crop-image/static/js/2.d253c45c.chunk.js"
+  },
+  {
+    "revision": "af56ac298e591128c9b8758e78861fc1",
+    "url": "/crop-image/static/js/2.d253c45c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aaf7145f5eaca80036a9",
+    "url": "/crop-image/static/js/main.c20c1eeb.chunk.js"
+  },
+  {
+    "revision": "1d973ad736c312e209dc",
+    "url": "/crop-image/static/js/runtime-main.171c745f.js"
+  }
+]);
