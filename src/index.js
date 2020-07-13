9e3b4aa4346d391da60fc5624e64d099147331b5
Burak Sekili <buraksekili@gmail.com>
2020-07-13 03:36:29 +0300

    Added client router

--- src/index.js
+++ src/index.js
@@ -1,4 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import App from "./Components/App";
-ReactDOM.render(<App />, document.getElementById("root"));
+import "bootstrap/dist/css/bootstrap.min.css";
+import { About } from "./Components/About";
+import { Contact } from "./Components/Contact";
+import { Page404 } from "./Components/Page404";
+import { Layout } from "./Components/Layout";
+import { NavBar } from "./Components/NavBar";
+
+ReactDOM.render(
+    <React.Fragment>
+        <Router>
+            <NavBar />
+            <Layout>
+                <Switch>
+                    <Route exact path="/" component={App} />
+                    <Route path="/about" component={About} />
+                    <Route path="/contact" component={Contact} />
+                    <Route component={Page404} />
+                </Switch>
+            </Layout>
+        </Router>
+    </React.Fragment>,
+    document.getElementById("root")
+);
